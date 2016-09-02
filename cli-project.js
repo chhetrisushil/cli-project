fc3010960b03b760d6cfe7e4d970f0226864f58d
Sushil Chhetri <chhetrisushil@gmail.com>
2016-09-02 19:04:44 +0530

    user input when process is running
    Added a demo for user input when process is running.

--- cli-project.js
+++ cli-project.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 console.log('testing cli commands', process.argv);
+
+//This is to read the inputs given when the process is running without exit
+/*process.stdin.on('data', function () {
+    console.log(arguments[0].toString());
+});*/
+
 process.exit();
